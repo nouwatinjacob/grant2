6c25f79ddae69b0bf94c9afe6c4f371980349e1f
Jacob Nouwatin <jaysansa@gmail.com>
2021-07-02 21:54:20 +0100

    add git ignore

--- myenv/lib/python3.6/site-packages/djrill/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._version import __version__, VERSION
-from .exceptions import (MandrillAPIError, MandrillRecipientsRefused,
-                         NotSerializableForMandrillError, NotSupportedByMandrillError)
